fd27ff93c0deb0d1dace6c532b0b6b5e84aaf4af
Victor-Emmanuel PROVOST <victor-emmanuel.provost@epita.fr>
2023-02-06 16:16:30 +0100

    update(config): update i3/polybar config

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 # Set simlink for i3 config
 ln -sf "$PWD/i3/config" "$HOME/.config/i3/config"
 #Set simlink for polybar
+mkdir -p "$HOME/.config/polybar/verdy"
 ln -sf "$PWD/polybar/config" "$HOME/.config/polybar/config"
 
 # Set simlink for rofi
