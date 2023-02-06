867aa83d743ece7bd7cd818bb8f71a40134e2e7b
Victor-Emmanuel PROVOST <victor-emmanuel.provost@epita.fr>
2023-02-06 16:12:42 +0100

    feat(install): add script to set up config files.

---
+++ install.sh
@@ -0,0 +1,8 @@
+# Set simlink for i3 config
+ln -sf "$PWD/i3/config" "$HOME/.config/i3/config"
+#Set simlink for polybar
+ln -sf "$PWD/polybar/config" "$HOME/.config/polybar/config"
+
+# Set simlink for rofi
+mkdir -p "$HOME/.config/rofi/verdy"
+ln -sf "$PWD/rofi/*" "$HOME/.config/rofi/verdy/"
