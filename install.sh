85af9d67e770c5be5c7c84f97dc8e7bf62e6f710
Victor-Emmanuel PROVOST <victor-emmanuel.provost@epita.fr>
2023-02-06 17:54:51 +0100

    feat(rofi): add files for rofi.

--- install.sh
+++ install.sh
@@ -3,13 +3,14 @@ SCRIPT_WD="$(dirname $(realpath "$BASH_SOURCE"))"
 # Copy the background to ~/Pictures to be used by i3
 cp "$SCRIPT_WD/img/bloom.png" "$HOME/Pictures/"
 
-# Set simlink for i3 config
+# Set symlink for i3 config
 ln -sf "$SCRIPT_WD/i3/config" "$HOME/.config/i3/config"
-# Set simlink for polybar
+# Set symlink for polybar
 mkdir -p "$HOME/.config/polybar/verdy"
 ln -sf "$SCRIPT_WD/polybar/config.ini" "$HOME/.config/polybar/verdy/config.ini"
 
-# Set simlink for rofi
+# Set symlink for rofi
 mkdir -p "$HOME/.config/rofi/verdy"
 ln -sf "$SCRIPT_WD/rofi/theme.rasi" "$HOME/.config/rofi/verdy/theme.rasi"
+ln -sf "$SCRIPT_WD/rofi/colors.rasi" "$HOME/.config/rofi/verdy/colors.rasi"
 ln -sf "$SCRIPT_WD/rofi/launcher.sh" "$HOME/.config/rofi/verdy/launcher.sh"
