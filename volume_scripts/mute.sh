07433ff9106811b6d1b6b4d1b82911b99426079f
Victor-Emmanuel PROVOST <victor-emmanuel.provost@epita.fr>
2023-02-07 15:36:05 +0100

    fix(volume): fix mute / unmute behaviour.

--- volume_scripts/mute.sh
+++ volume_scripts/mute.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
+vol=`amixer get Master | grep -m 1 "\[on\]" | sed -Ee 's/.*\[(.+)%.*/\1/'`
 CURRENT_STATE=`amixer get Master | egrep -m 1 'Playback.*?\[o' | egrep -o '\[on\]'`
-echo "$CURRENT_STATE"
 if [[ "$CURRENT_STATE" == "[on]" ]]; then
-    notify-send -t 1500 -u normal -h string:x-canonical-private-synchronous:anything "Sound muted"
+    notify-send -t 1500 -u normal -h string:x-canonical-private-synchronous:anything "sound muted - current volume: $vol%"
     amixer set Master mute
 else
-    notify-send -t 1500 -u normal -h string:x-canonical-private-synchronous:anything "Sound unmuted"
+    vol=`amixer get Master | grep -m 1 "\[off\]" | sed -Ee 's/.*\[(.+)%.*/\1/'`
+    notify-send -t 1500 -u normal -h string:x-canonical-private-synchronous:anything "sound unmuted - current volume: $vol%"
     amixer set Master unmute
 fi
--- volume_scripts/volume-decrease.sh
+++ volume_scripts/volume-decrease.sh
@@ -3,4 +3,11 @@
 amixer sset Master 5%- > /dev/null
 
 vol=`amixer get Master | grep -m 1 "\[on\]" | sed -Ee 's/.*\[(.+)%.*/\1/'`
-notify-send -t 1500 -u normal -h string:x-canonical-private-synchronous:anything "current volume: $vol%"
+
+if [ -z "$vol" ]; then
+    vol=`amixer get Master | grep -m 1 "\[off\]" | sed -Ee 's/.*\[(.+)%.*/\1/'`
+    notify-send -t 1500 -u normal -h string:x-canonical-private-synchronous:anything "sound unmuted - current volume: $vol%"
+    amixer set Master unmute
+else
+    notify-send -t 1500 -u normal -h string:x-canonical-private-synchronous:anything "current volume: $vol%"
+fi
